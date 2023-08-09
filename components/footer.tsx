b189bd55da9f507b2495317aebea725c63301a1e
dutrieuxl <dutrieuxbuisl@gmail.com>
2023-08-09 11:08:55 -0500

    deleted bottom piece

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,28 +10,6 @@ const Footer = () => {
   return (
     <div className="bg-amber-100 border-t-4 border-black py-12 px-4">
       <div className="max-w-xl mx-auto flex flex-col gap-6 text-center">
-        <p className="text-lg">
-          Made with {randomMadeWith} •{' '}
-          <span className="underline underline-offset-4 decoration-2">
-            <a
-              href="https://github.com/purduehackers/site"
-              target="_blank"
-              className="decoration-amber-400 dark:decoration-amber-500 hover:decoration-[3px]"
-            >
-              Open source
-            </a>
-          </span>{' '}
-          •{' '}
-          <span className="underline underline-offset-4 decoration-2">
-            <a
-              href="https://vercel.com?utm_source=purdue-hackers&utm_campaign=oss"
-              target="_blank"
-              className="decoration-amber-400 dark:decoration-amber-500 hover:decoration-[3px]"
-            >
-              Powered by ▲Vercel
-            </a>
-          </span>
-        </p>
         <FooterLinks />
       </div>
     </div>
