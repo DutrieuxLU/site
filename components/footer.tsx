7ee17e949762f5b059c79e8a2d3ba23903b6483e
dutrieuxl <dutrieuxbuisl@gmail.com>
2023-08-09 11:10:29 -0500

    heki

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,12 +3,8 @@ import { madeWith } from '../utils/data'
 import FooterLinks from './footer-links'
 
 const Footer = () => {
-  const [randomMadeWith, setRandomMadeWith] = useState('')
-  useEffect(() => {
-    setRandomMadeWith(madeWith[Math.floor(Math.random() * madeWith.length)])
-  }, [])
   return (
-    <div className="bg-amber-100 border-t-4 border-black py-12 px-4">
+    <div className="bg-[#fefae0] border-t-4 border-black py-12 px-4">
       <div className="max-w-xl mx-auto flex flex-col gap-6 text-center">
         <FooterLinks />
       </div>
