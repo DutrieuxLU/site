eb8d0e67d388bee8f0264ff3dfc94332b7797f7a
dutrieuxl <dutrieuxbuisl@gmail.com>
2023-08-09 11:07:19 -0500

    Added links

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,6 +8,8 @@ const Hero = () => {
   return (
     <div className="flex justify-start py-12 lg:py-10 lg:h-screen bg-[#fefae0]">
         <div className="p-4 ml-2 border-4 border-black rounded-sm shadow-blocks shadow-gray-800 bg-[#bc6c25]">
+
+
           <h1 className="text-6xl text-[#dda15e] font-bold text-left sm:text-8xl lg:text-9xl">
             Lukas Dutrieux's Homepage
           </h1>
@@ -18,15 +20,23 @@ const Hero = () => {
               className="ml-4 mt-10 sw-40 h-40 lg:w-60 lg:h-60 py-2 px-2 border-4 border-black rounded-sm shadow-blocks shadow-gray-800 bg-[#606c38]"
             />
           </div>
+        
+        
         </div>
-      <div className="ml-4"> {/* Add margin to separate the image */}
-      </div>
+      <div className="ml-4"></div>
       <div className="flex flex-col items-center w-10/14 mx-auto my-auto w-1/2">
         <Draggable disabled={!draggable}>
           <div className="p-4 bg-[#283618] border-4 border-black rounded-sm shadow-blocks shadow-gray-800 mb-4">
-            <p className="text-lg font-bold sm:text-xl text-[#606c38]">
-              (Inspired and Influenced by https://www.purduehackers.com/)
-            </p>
+          <h3 className="text-lg font-bold sm:text-xl text-[#606c38]">
+            <a
+              href="http://www.purduehackers.com"
+              target="_blank"  // Open link in a new tab
+              rel="noopener noreferrer" // Recommended for security
+              className="text-[#d9f99d] hover:underline"
+            >
+              Inspired by 🐐🐐🐐🐐
+            </a>
+          </h3>
           </div>
         </Draggable>
       </div>
