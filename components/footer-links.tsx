97e12d5dc572af54b35bd55628c8c0bba09a6ff2
dutrieuxl <dutrieuxbuisl@gmail.com>
2023-08-10 09:24:10 -0500

    added custom links

--- components/footer-links.tsx
+++ components/footer-links.tsx
@@ -2,15 +2,15 @@ import {
   Instagram,
   Discord,
   Github,
-  Twitter,
+  Linkedin,
   Mastodon
 } from '@icons-pack/react-simple-icons'
 
 const FooterLinks = () => (
   <div className="flex flex-row justify-center gap-x-8">
     <a
-      href="https://github.com/purduehackers"
-      className="transition hover:scale-110"
+      href="https://github.com/DutrieuxLU"
+      className="transition hover:scale-110 hover:text-[#3d3e40]"
       target="_blank"
     >
       <Github height={32} width={32} />
@@ -23,21 +23,21 @@ const FooterLinks = () => (
       <Discord height={32} width={32} />
     </a>
     <a
-      href="https://instagram.com/purduehackers"
+      href="https://www.instagram.com/lukas.dutrieux/?next=%2F"
       className="transition hover:scale-110 hover:text-[#c32aa3]"
       target="_blank"
     >
       <Instagram height={32} width={32} />
     </a>
     <a
-      href="https://twitter.com/purduehackers"
+      href="https://www.linkedin.com/in/lukas-dutrieux/"
       className="transition hover:scale-110 hover:text-[#1DA1F2]"
       target="_blank"
     >
-      <Twitter height={32} width={32} />
+      <Linkedin height={32} width={32} />
     </a>
     <a
-      href="https://mastodon.boiler.social/@purduehackers"
+      href="https://mastodon.social/@DutrieuxL"
       rel="me"
       className="transition hover:scale-110 hover:text-[#563acc]"
       target="_blank"
